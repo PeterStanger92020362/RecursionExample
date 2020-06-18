42ffb82ed270162f476d5a0d1e90adc483561a57
PeterStanger92020362 <psta191@mywhitecliffe.com>
2020-06-18 20:08:04 +1200

    program finished

--- src/RecursionExample.java
+++ src/RecursionExample.java
@@ -1,2 +1,22 @@
 public class RecursionExample {
+
+    public static void main(String[] args){
+
+        System.out.println("The countdown has begun");
+        countDown(10);
+        System.out.println("blastoff");
+
+
+    }
+
+    public static void countDown(int count){
+
+        if(count == 0){
+            return;
+        }
+        System.out.println(count + "...");
+        countDown(count-1);
+
+    }
+
 }
